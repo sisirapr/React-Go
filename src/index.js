2412d417d0877ecb3de5094839495326e5c88b78
sisira.pr <sisira.pr@thoughtworks.com>
2022-11-09 13:04:09 +0530

    Functional components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import AppHeader from './AppHeader';
-import AppFooter from './AppFooter';
+// import AppFooter from './AppFooter';
 import AppContent from './AppContent';
+import AppFooterFunctionalComponent from './AppFooterFunctionalComponent';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -29,11 +30,10 @@ class App extends Component {
     return (
       <div className='app'>
 
-        {/* <AppHeader title="My App"/> */}
         <AppHeader {...myProps} posts={this.state.posts} handlePostChange={this.handlePostChange} />
         <AppContent handlePostChange={this.handlePostChange} />
-        <AppFooter />
-
+        {/* <AppFooter /> */}
+        <AppFooterFunctionalComponent myProp={"Hello, world!"}/>
       </div>
     );
   }
